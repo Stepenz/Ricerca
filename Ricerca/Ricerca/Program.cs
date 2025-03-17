a8a53852d637bcec0338e9c4d26a8552a5607af4
Stepenz <spolaor.stefano.22@itisriva.edu.it>
2025-03-17 15:54:12 +0100

    Impostazione del menu

--- Ricerca/Ricerca/Program.cs
+++ Ricerca/Ricerca/Program.cs
@@ -2,9 +2,21 @@
 {
     internal class Program
     {
+        static int[] array = { 4, 6, 2, 7, 5, 9, 12 };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int scelta;
+            do
+            {
+                Console.WriteLine("---MENU---");
+                Console.WriteLine("1. Visualizzare l'array");
+                Console.WriteLine("2. Ricerca un numero");
+                Console.WriteLine("3. Esci");
+
+                scelta = int.Parse(Console.ReadLine());
+
+                
+            } while (true);
         }
     }
 }
